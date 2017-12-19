a77fe57643976182ee662e93115789a80da805ec
Jesus <jesus.tejedor.ovejas@gmail.com>
2017-12-19 20:36:20 +0100

    19/12

--- Ejercicio1/src/indicador.java
+++ Ejercicio1/src/indicador.java
@@ -1,46 +1,85 @@
 
 public class indicador {
-	
-	private int valormaximo;
-	private int valorminimo;
-	private int valoractual;
-	
-	
+    private int capacidadmax=0;
+    private int capacidadmin=0;
+    private int valorActual=0;
+    private double resultado;
 
-	
-	//Método para obtener el valor maximo
-		public int getvalormaximo()
-		{
-			return valormaximo;
+    public int incrementarUnValor(int cantidad){
+	int valorSumado=0;
+	//String valor;
+	if(cantidad>0){
+	    valorSumado=cantidad;
+	    if(valorActual + cantidad > capacidadmax){
+		    valorSumado = (capacidadmax - valorActual);
+		    valorActual = capacidadmax;
+		}else{
+		    valorActual = valorActual +cantidad;
 		}
-		//Método para establecer el valor maximo
-		public void setvalormaximo(int nuevovalormaximo)
-		{
-			valormaximo = nuevovalormaximo;
+	}
+	//valor = String.valueOf(valorSumado);
+	return valorSumado;
+    }//incrementarUnValor
+    //-------------------------------------------------------------------
+    public int reduceUnValor(int cantidad){
+	int valorRestado=0;
+	//String valor;
+	if(cantidad>0){
+	    valorRestado=cantidad;
+	    if(valorActual - cantidad < capacidadmin){
+		    valorRestado = valorActual;
+		    valorActual = capacidadmin;
+		}else{
+		    valorActual = valorActual -cantidad;
 		}
-		public int getvalorminimo()
-		{
-			return valorminimo;
-		}
-		//Método para establecer el valor minimo
-		public void setvalorminimo(int nuevovalormaximo)
-		{
-			valorminimo = nuevovalormaximo;
-		}
-		public int getvaloractual()
-		
-		{
-			valoractual=20;
-			return valoractual;
-		}
-		//Método para establecer el valor minimo
-		public void setvaloractual(int nuevovaloractual)
-		{
-			valoractual = nuevovaloractual;
-		}
-		
+	}
+	//valor = String.valueOf(valorSumado);
+	return valorRestado;
+    }//incrementarUnValor
+    //-------------------------------------------------------------------
+    public int getCapacidadmax() {
+	return capacidadmax;
+    }
 
-		}
-	
+    public void setCapacidadmax(int capacidadmax) {
+	this.capacidadmax = capacidadmax;
+    }
 
 
+
+    public int getCapacidadmin() {
+	return capacidadmin;
+    }
+
+
+
+    public void setCapacidadmin(int capacidadmin) {
+	this.capacidadmin = capacidadmin;
+    }
+
+
+
+    public int getValorActual() {
+	return valorActual;
+    }
+
+
+
+    public void setValorActual(int valorActual) {
+	if(valorActual > capacidadmax){
+	    this.valorActual = capacidadmax;
+	}else{
+	    this.valorActual = valorActual;
+	}
+    }//
+
+
+
+    public double getResultado() { //Porcentaje
+	if (valorActual>=capacidadmin && valorActual<=capacidadmax){
+	    resultado = ((double)valorActual/capacidadmax)*100;
+	}
+	return (int)resultado; //Muestra el porcentaje en caso de que este bien escrito
+    }
+
+}
--- Ejercicio1/src/indicadortest.java
+++ Ejercicio1/src/indicadortest.java
@@ -11,27 +11,61 @@ import java.util.*;
  */
 public class indicadortest {
 
+    public static void main(String[] args) {
+	@SuppressWarnings("resource")
+	Scanner teclado = new Scanner(System.in);
+	indicador deposito;
+	int op=0;
+	int datos; //Una variable de tipo int para que envie los datos al constructor
+	deposito = new indicador();
+	//String cadenaDeIncrementar;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner teclado = new Scanner (System.in); 
-		
-		int nuevovaloractual;
-		int litrosgastados;
-		
-		indicador gasolina;
-		gasolina=new indicador();
-		
-	
-		System.out.println( "  tenias 20 litros ¿cuantos litros has hechado? ");
-		nuevovaloractual=teclado.nextInt();
-		System.out.println(" ahora tienes " +(gasolina.getvaloractual()+nuevovaloractual));
-		
-		System.out.println("cuantos litros has gastado");
-		litrosgastados=teclado.nextInt();
-		System.out.println(" ahora tienes " +(gasolina.getvaloractual()+nuevovaloractual-litrosgastados));
-		
-		
-	}
+	System.out.println("Indica el tamaño maximo del deposito: ");
+	datos = teclado.nextInt();
+	deposito.setCapacidadmax(datos);
+	op = menu("Elige que desea hacer: :\n 1. Añadir liquido :\n 2. Extraer liquido :\n 0. Salir");
 
-}
+	while(op!=0){
+	    switch (op){
+	    case 1:
+		System.out.println("¿Cuanto quieres añadir?");
+		datos = teclado.nextInt();
+		System.out.println("La cantidad que se ha añdido es: " + deposito.incrementarUnValor(datos));
+		break;
+	    case 2:
+		System.out.println("¿Cuanto quieres quitar?");
+		datos = teclado.nextInt();
+		System.out.println("La cantidad que se ha extraido es: " + deposito.reduceUnValor(datos));
+		break;
+	    default:
+		System.out.println("opcion no valida"); 
+	    }
+	    System.out.println("Lo maximo que puede almacenar: "+deposito.getCapacidadmax());
+	    System.out.println("La cantidad que contiene: "+deposito.getValorActual());
+	    System.out.println("El porcentaje de ocupación es: "+deposito.getResultado()+"% \n");
+
+	    op = menu("Elige que desea hacer: :\n 1. Añadir liquido :\n 2. Extraer liquido :\n 0. Salir");
+	}//while
+	System.out.println("Se ha finalizado el programa.");
+
+    }//main
+
+    public static int menu(String mensaje){
+	@SuppressWarnings("resource")
+	Scanner teclado = new Scanner(System.in);
+	int op;
+	System.out.println(mensaje);
+	op = teclado.nextInt(); 
+	return op;
+    }
+
+
+    public static int pedirEntero(String mensaje){
+	@SuppressWarnings("resource")
+	Scanner teclado = new Scanner(System.in);
+	int n;
+	System.out.println(mensaje);
+	n = teclado.nextInt();
+	return n;
+    }//pedirEntero
+}//class
